3d774576c77966af81c92e1be34e00756bd940eb
GussRw <gussrw1@gmail.com>
2018-08-10 17:48:15 -0500

    Composer project was created

---
+++ src/AccountVerificationServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MyShell\AccountVerification;
+
+use Illuminate\Support\ServiceProvider;
+
+class AccountVerificationServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
