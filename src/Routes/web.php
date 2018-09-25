64c5d74d0fd528da11c8371d19900eb5fdd70cf9
Gustavo Peralta Vazquez <GussRw1@Gmail.com>
2018-09-25 07:41:43 -0500

    fix error on config names

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -8,16 +8,16 @@ Route::group(['middleware' => ['web']], function () {
         if ($unverified_account == null)
             return abort(404);
 
-        if (config('account_verification.before verification call back ', false))
-            app()->call(config('account_verification.before verification call back '));
+        if (config('account_verification.before_verification_callback ', false))
+            app()->call(config('account_verification.before_verification_callback '));
         $account = $unverified_account->account;
         $unverified_account->delete();
         
         if (config('account_verification.login_after_verification', true))
             \Illuminate\Support\Facades\Auth::guard('web')->login($account);
 
-        if (config('account_verification.after verification call back ', false))
-            app()->call(config('account_verification.before verification call back '));
+        if (config('account_verification.after_verification_callback ', false))
+            app()->call(config('account_verification.after_verification_callback '));
 
         return redirect(config('account_verification.redirect_to', '/'));
     })->name('account.verify');
